d74daf92a8f6878424ca6c0c4cf312413ecb787b
Ilan <ilan.vasilevsky@gmail.com>
2022-06-08 18:52:57 +0300

    implemented get data

--- part2/utils.py
+++ part2/utils.py
@@ -1,3 +1,25 @@
+import pandas as pd
+
+from config import TRAIN_PATH, USER_COL_NAME_IN_DATAEST, ITEM_COL_NAME_IN_DATASET, USER_COL, ITEM_COL, VALIDATION_PATH, \
+    RATING_COL, RATING_COL_NAME_IN_DATASET
+
+
+def transform_data_to_internal_indexes(data: pd.DataFrame, user_map: Dict, item_map: Dict) -> pd.DataFrame:
+    data[USER_COL] = data[USER_COL_NAME_IN_DATAEST].map(user_map)
+    data[ITEM_COL] = data[ITEM_COL_NAME_IN_DATASET].map(item_map)
+    data[RATING_COL] = data[RATING_COL_NAME_IN_DATASET]
+    return data[[USER_COL,ITEM_COL, RATING_COL]]
+
+
+def get_user_and_item_map(data: pd.DataFrame):
+    data[USER_COL] = pd.factorize(data[USER_COL_NAME_IN_DATAEST])[0]
+    data[ITEM_COL] = pd.factorize(data[ITEM_COL_NAME_IN_DATASET])[0]
+    user_map = data[[USER_COL, USER_COL_NAME_IN_DATAEST]].drop_duplicates()
+    user_map = user_map.set_index(USER_COL_NAME_IN_DATAEST).to_dict()[USER_COL]
+    item_map = data[[ITEM_COL, ITEM_COL_NAME_IN_DATASET]].drop_duplicates()
+    item_map = item_map.set_index(ITEM_COL_NAME_IN_DATASET).to_dict()[ITEM_COL]
+    return user_map, item_map
+
 def get_data():
     """
     reads train, validation to python indices so we don't need to deal with it in each algorithm.
@@ -5,8 +27,18 @@ def get_data():
     if in the validation set there is an index that does not appear in the train set then we can put np.nan or
      other indicator that tells us that.
     """
-    # return train, validation
-    raise NotImplementedError
+    # read train data and remap indexes 
+    train = pd.read_csv(TRAIN_PATH)
+    user_map, item_map = get_user_and_item_map(train)
+    train = transform_data_to_internal_indexes(train, user_map, item_map)
+    # read validation data and remap indexes
+    validation = pd.read_csv(VALIDATION_PATH)
+    validation = transform_data_to_internal_indexes(validation, user_map, item_map)
+    # convert to numpy arrays
+    train = train.values.astype(int)
+    validation = validation.values.astype(int)
+    
+    return train, validation
 
 
 class Config:
